c99f9d507d896fbc6d9e76752760a9dc1d4691ed
Yann <ychantrel@gmail.com>
2021-02-22 13:16:36 +0100

    appli v9 : le serveur ne ce ferme plus si le client part

--- Serveur_TCP/src/Main.java
+++ Serveur_TCP/src/Main.java
@@ -4,6 +4,7 @@
  * */
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.ServerSocket;
@@ -11,29 +12,42 @@ import java.net.Socket;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		System.out.println("Le Serveur FTP");
 		
-		ServerSocket serveurFTP = new ServerSocket(4500);
-		Socket socket = serveurFTP.accept();
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		PrintStream ps = new PrintStream(socket.getOutputStream());
-		
-		ps.println("1 Bienvenue ! ");
-		ps.println("1 Serveur FTP Personnel.");
-		ps.println("0 Authentification : ");
-		
-		String commande = "";
-		
-		// Attente de reception de commandes et leur execution
-		while(!(commande=br.readLine()).equals("bye")) {
-			System.out.println(">> "+commande);
-			CommandExecutor.executeCommande(ps, commande);
+		ServerSocket serveurFTP = null;
+		Socket socket = null;
+		try {
+			serveurFTP = new ServerSocket(4500);
+			socket = serveurFTP.accept();
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			PrintStream ps = new PrintStream(socket.getOutputStream());
+			
+			ps.println("1 Bienvenue ! ");
+			ps.println("1 Serveur FTP Personnel.");
+			ps.println("0 Authentification : ");
+			
+			String commande = "";
+			
+			// Attente de reception de commandes et leur execution
+			while(!(commande=br.readLine()).equals("bye")) {
+				System.out.println(">> "+commande);
+				CommandExecutor.executeCommande(ps, commande);
+			}
+			
+			serveurFTP.close();
+			socket.close();
+		} catch (IOException e) {
+			System.out.println("Le client c'est déconnecté.");
+			try {
+				serveurFTP.close();
+				socket.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			main(args);
 		}
-		
-		serveurFTP.close();
-		socket.close();
 	}
 
 }
