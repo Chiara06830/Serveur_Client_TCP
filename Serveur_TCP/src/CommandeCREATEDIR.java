845d5498adb35f9e2c1acd2edf695436ca218052
aVadeleau <alanvadeleau@live.fr>
2021-03-16 08:22:48 +0100

    Modifs createdir

--- Serveur_TCP/src/CommandeCREATEDIR.java
+++ Serveur_TCP/src/CommandeCREATEDIR.java
@@ -14,33 +14,48 @@ public class CommandeCREATEDIR extends Commande {
 		
 		String testPath[] = commandeArgs[0].split("\\"+ File.separator);
 		System.out.println(unClient.getEmplacement());
-		if(	!((unClient.getEmplacement().equals("personne")) && (testPath[0].equals(".."))) ) {
-			File newFile = new File(unClient.getEmplacement()+ File.separator + commandeArgs[0]);
-			System.out.println(newFile);
-			System.out.println(newFile.exists());
-			
-			if(!(newFile.exists())) {
-				System.out.println("Ici");
-			
-				if(newFile.mkdirs()) {
-					System.out.println("La");
-					ps.println("0 Le répertoire "+newFile+" a été créé");			
-					
+		if(!(commandeArgs[0].equals(File.separator) && !(commandeArgs[0].equals("~")))) {
+			if(	!((unClient.getEmplacement().equals("personne")) && (testPath[0].equals(".."))) ) {
+				File newFile = new File(unClient.getEmplacement()+ File.separator + commandeArgs[0]);
+				System.out.println(newFile);
+				System.out.println(newFile.exists());
+				
+				if(!(newFile.exists())) {
+					System.out.println("Ici");
+				
+					if(newFile.mkdirs()) {
+						System.out.println("La");
+						ps.println("0 Le répertoire "+newFile+" a été créé");			
+						
+					}
+					else {
+						ps.println("2 La syntaxe est incorrecte");
+					}			
 				}
 				else {
-					ps.println("2 La syntaxe est incorrecte");
-				}			
-			}
-			else {
-				ps.println("2 Le répertoire existe dèjà ou le nom n'est pas disponible");
+					ps.println("2 Le répertoire existe dèjà ou le nom n'est pas disponible");
+					
+				}
+				
 				
 			}
+			else {
+				ps.println("2 Vous ne pouvez pas créer de répertoire ");
+			}
+			
+			
+			
+			
+			
 			
 			
 		}
 		else {
-			ps.println("2 Vous ne pouvez pas créer de répertoire ");
+			ps.println("2 Ce nom de répertoire n'est pas autorisé");
+			
+			
 		}
+		
 
 		
 		
